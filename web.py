491c67b81c555c54f20b6c1e2a694e7b35c7067a
WYudin <bevqntb@gmail.com>
2023-04-10 20:27:29 +0300

    Finish /services

--- web.py
+++ web.py
@@ -1,13 +1,9 @@
 import os
+import datetime as dt
 import random
-import time
-import flask.wrappers
-import requests
 from flask import Flask, render_template, redirect
 from data.services_town_ask import TownForm
 from functions import search
-import asyncio
-import schedule
 
 app = Flask(__name__)
 secret_key = "%032x" % random.getrandbits(128)
@@ -32,51 +28,16 @@ def services(town):
     return render_template('services.html', title=title, services=services)
 
 
-# @app.teardown_request
-# def add_header(response):
-#     global TEARDOWN_COUNTER
-#     TEARDOWN_COUNTER += 1
-#     if TEARDOWN_COUNTER % 4 == 0 and TEARDOWN_COUNTER != 0:
-#         for currentdir, dirs, files in os.walk('static/hash'):
-#             for k in files:
-#                 os.remove(currentdir + '/' + k)
-#         print(123)
-#
-#     print(TEARDOWN_COUNTER)
-#     return response
+@app.after_request
+def after_request(response):
+    for currentdir, dirs, files in os.walk('static/cash'):
+        for k in files:
+            create_data = dt.datetime.fromtimestamp(os.path.getctime('static/cash/' + k))
+            a = dt.datetime.now() - create_data
+            if a >= dt.timedelta(minutes=1):
+                os.remove('static/cash/' + k)
+    return response
 
-# @app.after_request
-# def aaa_help_pls(response):
-#     if response.is_streamed:
-#         time.sleep(5)
-#         # photo = list(response.iter_encoded())[0]
-#         for currentdir, dirs, files in os.walk('static/cash'):
-#             for k in files:
-#                 print(k)
-#                 # with open(currentdir + '/' + k, mode='rb') as file:
-#                 #     # if file == photo:
-#                 os.remove(currentdir + '/' + k)
-#     print(response)
-#     return response
-
-
-# def cash_delete():
-#     print(123)
-
-
-#
-#
-# async def await_func():
-#     await asyncio.gather(asyncio.create_task(cash_delete()))
-
-# schedule.every(1).seconds.do(cash_delete)
 
 if __name__ == '__main__':
-    while True:
-        schedule.run_pending()
-        app.run(port=8080, host='127.0.0.1')
-    # if os.name == 'nt':
-    #     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    #
-    # asyncio.run(await_func())
-
+    app.run(port=8080, host='127.0.0.1')
