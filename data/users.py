34b08567287f1ab6b9a25fa17d0131be516b1150
WYud1n <bevqntb@gmail.com>
2023-04-12 23:44:57 +0300

    New branch for account check

---
+++ data/users.py
@@ -0,0 +1,20 @@
+import datetime
+import sqlalchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    login = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    username = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print('The START of TLC-club')
+from flask import Flask, render_template, redirect
+# from data import db_session
+from data.users import User
+from mailer import send_email, EMailText
+import datetime
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(days=365)
+
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+
+
+def main():
+    # db_session.global_init("db/mars.db") # Тут тож допиши че нить
+    app.run()
+
+
+@app.route('/account/<int:userid>')
+def account(userid):
+    # Тут крч должен быть запрос в БД с поиском юзера с указанным ID
+    # Ниже будет User для теста
+    u = User
+    u.username = 'Sirenogoloviy'
+    u.email = 's.tiktok-trend@podkraduli.ru'
+    # тут крч должен быть поиск в бд по id юзера всяких там сообщений и тд ну ты пон
+
+    return render_template('account.html', title=f'Аккаунт {u.username}', user=u)
+
+
+main()
